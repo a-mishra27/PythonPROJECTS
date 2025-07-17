05ecbbf01c3e99085143606c2dc2b387fe97b52b
a-mishra27 <amishamishra278@gmail.com>
2025-07-17 20:28:34 +0530

    Add files via upload

---
+++ tic tac toe.py
@@ -0,0 +1,26 @@
+import math
+import random
+
+class Player:
+    def _init_(self, letter):
+        self.letter = letter
+
+
+    def get_move(self ,game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def _init_(self , letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def _init_(self, letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        pass
+
+
